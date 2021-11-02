b7cf649219b89d09ae897295837b82dce8398a1c
jalakhras <jalakhras@outlook.com>
2021-11-03 00:48:40 +0200

    Append text bpx above  the line

--- GlobomanticsElectricCompany.BillProcessor/Builder/PaymentStubPerforatedLineBuilder.cs
+++ GlobomanticsElectricCompany.BillProcessor/Builder/PaymentStubPerforatedLineBuilder.cs
@@ -20,6 +20,27 @@ namespace GlobomanticsElectricCompany.BillProcessor.Builder
             };
 
             builder.InsertNode(line);
+
+            var lineMessageTextBox = new Shape(builder.Document, ShapeType.TextBox)
+            {
+                RelativeHorizontalPosition = RelativeHorizontalPosition.Margin,
+                RelativeVerticalPosition = RelativeVerticalPosition.Margin,
+                Top = 510,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Stroked = false,
+                Width = 450,
+                Height = 20
+            };
+
+            var lineMessagePargraph = new Paragraph(builder.Document);
+            var lineMessageRun = new Run(builder.Document, "PLEASE DETACH LOWER PORTION" +" AND RETURN WITH PAYMENT");
+            lineMessageRun.Font.Name = GlobalDocumentSettings.FontName;
+            lineMessageRun.Font.Size = GlobalDocumentSettings.FontSize;
+            lineMessagePargraph.AppendChild(lineMessageRun);
+            lineMessagePargraph.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+            lineMessageTextBox.AppendChild(lineMessagePargraph);
+            builder.Document.FirstSection.Body.FirstParagraph.AppendChild(lineMessageTextBox);
         }
     }
 }
